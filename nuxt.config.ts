0f1313270d452363c065a329ba33477d33d20a48
Connor Christensen <connor@funda.nl>
2024-05-27 15:02:27 +0200

    Install Nuxt UI Pro
    Following the instructions on https://ui.nuxt.com/pro/getting-started/installation:
    
    1. `npm install @nuxt/ui-pro`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/storybook"]
+  extends: [
+    '@nuxt/ui-pro'
+  ],
+  modules: [
+    '@nuxtjs/storybook',
+    '@nuxt/ui'
+  ]
 })
